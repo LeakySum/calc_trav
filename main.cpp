b64f95a52f169967c6c572a353a0e954ae493bce
LeakySum <leakysum60@gmail.com>
2016-10-20 22:39:15 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,6 @@
+double sum (double a, double b);
+double subtraction (double a, double b);
+double divison (double a, double b);
+double multiplication (double a, double b);
+double square (double a);
+double power (double a, unsigned int b);
