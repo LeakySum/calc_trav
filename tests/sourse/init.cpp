ad3c8fa07b9a0b7c25bfe64203d6d74aef593292
LeakySum <leakysum60@gmail.com>
2016-10-20 22:05:16 +0300

    Update init.cpp

--- tests/sourse/init.cpp
+++ tests/sourse/init.cpp
@@ -5,8 +5,13 @@ SCENARIO("calculator sum", "[sum]") {
     double x = 10;
     double y = 2;
     
-    double rv = sum ( x, y);
+    double sm = sum ( x, y);
+    double sb = subtraction ( x, y);
+    double dv = divison ( x, y);
+    double mp = multiplication ( x, y);
+    double sq = square ( x);
+    double p =  power ( x, y);
     
-    REQUIRE( rv == 7);
+    REQUIRE( sm == 12, sb == 8, dv == 5, mp == 20, sq == 3,16228, p == 100 );
 }
   
