8149f8e57ed1809f2a4366353dbdda62739100e5
LeakySum <leakysum60@gmail.com>
2016-11-10 21:17:51 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -1,6 +1,6 @@
 double sum (double a, double b);
-double subtraction (double a, double b);
-double divison (double a, double b);
-double multiplication (double a, double b);
+double sub (double a, double b);
+double divis (double a, double b);
+double mult (double a, double b);
 double square (double a);
 double power (double a, double b);
