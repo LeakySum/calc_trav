73533aa458f546dd5cdec15abca07611dd07e9c3
LeakySum <leakysum60@gmail.com>
2016-11-10 21:17:06 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -7,9 +7,9 @@ int main() {
     cout <<"Enter: a, b"<< endl;
     cin >> a >> b ;
     cout << "a+b=" << sum(a,b) << endl;
-    cout << "a-b=" << subtraction(a,b) << endl;
-    cout << "a/b=" << division(a,b) << endl;
-    cout << "a*b=" << multyplication(a,b) << endl;
+    cout << "a-b=" << sub(a,b) << endl;
+    cout << "a/b=" << divis(a,b) << endl;
+    cout << "a*b=" << mult(a,b) << endl;
     cout << "sqrt(a)=" << square(a) << endl;
     cout << "a^b=" << power (a,b) << endl;
     return 0;
