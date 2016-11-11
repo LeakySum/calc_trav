2c65b9f604d6ea04547126cfed034136a20cc689
LeakySum <leakysum60@gmail.com>
2016-11-11 13:24:39 +0400

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -1,4 +1,8 @@
+
 double sum (float a, float b);
 double sub (float a, float b);
 double div (float a, float b);
+double mult (float a, float b);
+double sq (float a);
+double powe (float a, float b);
 
