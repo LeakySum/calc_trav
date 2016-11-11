1ffa2044f06cf6c42779107edef3a53d2b9e4b30
LeakySum <leakysum60@gmail.com>
2016-11-11 13:25:55 +0400

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -12,3 +12,25 @@ double div (float a, float b){
         return (float)a/b;
 }
 
+double mult (float a, float b){
+    return a*b;
+}
+
+double sq (float a){
+    double x=1; double y;
+    int i=0;
+    while(1) {
+        y=x;
+        x=1./2*(x+a/x);
+        if (x>=y) { i++; if (i>1) { break;}};
+    }
+
+    return x;
+}
+double pow (float a, float b){
+    int x=a;
+    if (b==0) a=0; else{
+        for(int i=1; i<b; i++) a=a*x;
+    }
+    return (double) a;
+}
