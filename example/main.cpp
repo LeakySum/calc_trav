2ff4300fcd3bb896b5dd13abc3883fcd70169bdb
LeakySum <leakysum60@gmail.com>
2016-11-11 13:23:16 +0400

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 int main() {
-    float a, b;
-    cout <<"Enter: a, b"<< endl;
-    cin >> a >> b ;
-    cout << "a+b=" << sum(a,b) << endl;
-    cout << "a-b=" << sub(a,b) << endl;
-    cout << "a/b=" << div(a,b) << endl;
-
-    return 0;
+float a, b;
+cin >> a >> b ;
+cout << "a+b=" << sum(a,b) << endl;
+cout << "a-b=" << sub(a,b) << endl;
+cout << "a/b=" << div(a,b) << endl;
+cout << "a*b=" << mult(a,b) << endl;
+cout << "sqrt(a)=" << sq(a) << endl;
+cout << "a^b=" << pow (a,b) << endl;
+return 0;
 }
