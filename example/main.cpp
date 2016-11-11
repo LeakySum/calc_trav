3abd3707d7b8d0c07029e92d280c8822f53327de
LeakySum <leakysum60@gmail.com>
2016-11-11 13:51:11 +0400

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -10,6 +10,6 @@ cout << "a-b=" << sub(a,b) << endl;
 cout << "a/b=" << div(a,b) << endl;
 cout << "a*b=" << mult(a,b) << endl;
 cout << "sqrt(a)=" << sq(a) << endl;
-cout << "a^b=" << pow (a,b) << endl;
+cout << "a^b=" << power (a,b) << endl;
 return 0;
 }
